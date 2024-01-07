5e0de9a99941e86cd294cf493a172920887a2e34
Randy Knapp <randy.bravo2@gmail.com>
2024-01-07 11:39:00 -0800

    Cheaper Shotgun Shells & Location Based Beds

---
+++ SunkenlandMods/CheaperShotgunShells/CheaperShotgunShells.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CheaperShotgunShells
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class CheaperShotgunShells : BaseUnityPlugin
+    {
+        public const string GUID = "randyknapp.mods.cheapershotgunshells";
+        public const string NAME = "Cheaper Shotgun Shells";
+        public const string VERSION = "0.1.0";
+
+        //public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            //logger = Logger;
+
+            Logger.LogWarning($"{NAME} Loaded");
+            //Logger.LogWarning($"- {Config.Definition.Key}: {Config.Value}");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+        }
+    }
+
+    [HarmonyPatch]
+    public static class WorkstationPatch
+    {
+        private static bool _patchComplete = false;
+
+        [HarmonyPatch(typeof(Workstation), nameof(Workstation.Spawned))]
+        [HarmonyPrefix]
+        public static void Spawned_Prefix()
+        {
+            if (_patchComplete)
+                return;
+
+            var shotgunAmmoCraftable = RM.code.AllCraftables.FirstOrDefault(x => x.name == "Shotgun Ammo");
+            if (shotgunAmmoCraftable != null)
+            {
+                //CheaperShotgunShells.logger.LogWarning("Found Shotgun Shells!");
+                var req = shotgunAmmoCraftable.itemRequirements.FirstOrDefault(x => x.item.name == "A3_Components");
+                if (req != null)
+                {
+                    req.amount = 1;
+                    _patchComplete = true;
+                }
+                else
+                {
+                    //CheaperShotgunShells.logger.LogWarning("Did not find Component requirement!");
+                }
+            }
+            else
+            {
+                //CheaperShotgunShells.logger.LogWarning("Did not find Shotgun Shells!");
+            }
+        }
+    }
+}
---
+++ SunkenlandMods/NamedBeds/LocationBasedBeds.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using Fusion;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NamedBeds
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class LocationBasedBeds : BaseUnityPlugin
+    {
+        public const string GUID = "randyknapp.mods.locationbasedbeds";
+        public const string NAME = "Location Based Beds";
+        public const string VERSION = "0.1.0";
+
+        //public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            //logger = Logger;
+
+            Logger.LogWarning($"{NAME} Loaded");
+            //Logger.LogWarning($"- {Config.Definition.Key}: {Config.Value}");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+        }
+    }
+
+    [HarmonyPatch]
+    public static class UIDeathPatch
+    {
+        private static Location FindClosestLocation(Vector3 worldPos)
+        {
+            //LocationBasedBeds.logger.LogWarning("Find Closest Location:");
+            Location closest = null;
+            var distanceSq = float.MaxValue;
+            foreach (var location in WorldScene.code.locationsList)
+            {
+                var d = worldPos - location.mapLocator.position;
+                var dSq = d.sqrMagnitude;
+                //LocationBasedBeds.logger.LogWarning($"    {location.locationName}: {Mathf.Sqrt(dSq):N1}");
+                if (dSq < distanceSq)
+                {
+                    distanceSq = dSq;
+                    closest = location;
+                }
+            }
+
+            return closest;
+        }
+
+        [HarmonyPatch(typeof(UIDeath), nameof(UIDeath.Open))]
+        [HarmonyPostfix]
+        public static void Open_Postfix(UIDeath __instance)
+        {
+            var interactButtons = Traverse.Create(__instance).Field<List<Button>>("InteractBtns").Value;
+
+            for (var index = 0; index < WorldScene.code.Beds.Count; ++index)
+            {
+                var bed = WorldScene.code.Beds[index];
+                if (Mainframe.code.WorldManager.IsSpawnPointShared || !(bed.BuilderID != Mainframe.code.SaveManager.CurrentCharacterGuid))
+                {
+                    var button = interactButtons[index];
+                    var closestLocation = FindClosestLocation(bed.transform.position);
+                    button.GetComponentInChildren<Text>().text = closestLocation.locationName;
+                }
+            }
+        }
+    }
+}
